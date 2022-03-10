2cb3578cdf315565d453a96dda7700354812c053
Arjun <arjun.sharma@skuad.io>
2022-03-10 19:13:53 +0530

    Init

--- README.md
+++
@@ -1,6 +0,0 @@
-# Code-Wars-in-C
-30- day intense C training with all curated programs
-
-Contains all major building block source codes of the programming language C.
-
-Starting from Beginners to Advanced.
--- ShortCircuit.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-void main()
-{
-    int x = 1, y = 0, z = 5;
-    int a = x && y || z++;      //z++ will be evaluated
-    int b = x && y && z++;      //z++ will not be evaluated as LHS is by default 0 for logical AND
-    printf("%d", z);
-}
